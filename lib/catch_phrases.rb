0c70ba01e2a36824fcaa75041ecd24ad3b50f57e
Jose Mendoza <pball626@gmail.com>
2021-02-05 21:08:26 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,27 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!"
   puts phrase
-end
+  end
+  
+  def toadstool
+    status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+    puts status
+  end
 
-def toadstool
+def link
+  status = "It's Dangerous To Go Alone! Take This."
   puts status
 end
+
+def all_phrases
+  mario = "It's-a me, Mario!"
+  toadstool = "Thank You Mario! But Our Princess Is In Another Castle!"
+  link = "It's Dangerous To Go Alone! Take This."
+  puts "{Mario}, {Toadstool}, {Link}"
+end
+  
+  
+#status
+  #puts "puts out all of the previous catch phrases"
+  
